9f7896d69b1697778f81f117501b1c284e869fef
Radit <radityaoktavianor@gmail.com>
2025-03-10 12:42:54 +0800

    permulaan

---
+++ Index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html dir="ltr" class="Dark" style="color-scheme : dark; "lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head> IKAN SEPAT IKAN LELE <HR>
+<body>
+    
+</body>
+</html>
+
+<?php
+
+echo "ISAPKAN DULU LE" ;
+
+?>
