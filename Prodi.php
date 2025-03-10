144fb22b715b4f1214a94bc3b640e6dcc446a61e
Radit <radityaoktavianor@gmail.com>
2025-03-10 13:40:59 +0800

    Table

---
+++ Prodi.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html dir="ltr" class="Dark" style="color-scheme : dark;" lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIMPADU POLIBAN</title>
+</head>
+<tbody>
+    <h1>DATA MEHASISWA</h1>
+    <br>
+    <table border="1"
+        <thead>
+            <tr>
+            <th>NO</th>
+            <th>NIM</th>
+            <th>Nama</th>
+            </tr>
+        </thead>
+</tbody>
+            <tr>
+                <td>1</td>
+                <td>E020323102</td>
+                <td>Raditya Oktaviano</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>E020323101</td>
+                <td>Muhammad Satrio</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>JMK0203532</td>
+                <td>Amba Tungkang</td>
+            </tr>
+</html>
